1b52fe01dbbacac475ef74d6923e0ddc15b7ff5c
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 17:01:40 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,4 +12,10 @@ class Transfer
     sender.valid? and receiver.valid?
   end
 
+  def execute_transaction 
+    if sender.balance > transfer_amount
+      receiver.deposit(transfer_amount)
+    else 
+      "Transaction rejected. Please check your account balance."
+
 end
