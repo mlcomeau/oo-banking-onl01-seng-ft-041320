76a3410943864f68ba4b60f0b7a3d94b201cfd7c
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 16:52:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+  attr_accessor :status, :transfer_amount
+  attr_reader :sender, :receiver
+  def initialize (sender, receiver, transfer_amount)
+    @sender = sender 
+    @receiver = receiver 
+    @status = "pending"
+    @transfer_amount = transfer_amount
+  end 
+  
 end
