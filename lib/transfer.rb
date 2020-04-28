ff45403b9758e62f611c6749dc37279864409639
Megan Comeau <mlcomeau99@gmail.com>
2020-04-28 17:02:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,5 @@
 class Transfer
-  attr_accessor :status,
+  attr_accessor :status
   attr_reader :sender, :receiver, :transfer_amount
   def initialize (sender, receiver, transfer_amount)
     @sender = sender
